decf8d5ba906c4c578fe9457f434c0b6d394db6e
markup54 <marzenakupka@tlen.pl>
2025-02-04 07:58:03 +0100

    plan

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+    }
+    private static ArrayList<Integer> wylosujListe(int n, int a,int b){
+        ArrayList<Integer> wylosowana = new ArrayList<>();
+
+        return wylosowana;
+    }
+    private static int[] wylosujDoTablicy(int n,int a,int b){
+        int [] wylosowanaTablica = new int[n];
+
+        return wylosowanaTablica;
+    }
+    private static void  wypiszTablice (int[] tablicaDoWypisania){
+
+    }
+    private static int wyszukajLiniowo(int[] tablicaZLiczbami,int szukanaWartosc){
+
+        return -1;
+    }
+    private static int wyszukajLiniowo(ArrayList<Integer> listaZliczbami,int szukanaWartosc){
+
+        return -1;
+    }
+    private static int[] sortujBabelkowo(int[] tablicaLiczbowa){
+
+    }
+    private static int wyszukajBinarnie(int[] tablicaZLiczbami,int szukanaWartosc){
+        return -1;
+    }
+    private static int wyszukajBinarnie(ArrayList<Integer> listaZLiczbami,int szukanaWartosc){
+        return -1;
+    }
+}
